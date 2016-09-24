4822d83c949cd3a14f44e5b99e08a49c84c11a7b
Diomede Mazzone <diomede.mazzone@gmail.com>
2016-09-24 11:45:59 +0200

    add random function

--- leo/leo.ino
+++ leo/leo.ino
@@ -8,7 +8,7 @@ int echoPort = A5;     // il sensore echo viene collegato alla porta A5 .
 Servo serX;
 Servo serY;
 
-String msg;
+String msg, oldMsg;
 char messaggio[10];
 //AF_DCMotor motorS(1);      // Motore sinistro collegato al pin 1 . 
 //AF_DCMotor motorD(4);      // Motore destro collegato al pin 4 .
@@ -19,32 +19,63 @@ int Y, angY;
 
 void setup() {
 Serial.begin(9600); // set the baud rate
-serX.attach(9);
-serY.attach(10);
+  serX.attach(9);
+  serY.attach(10);
+  
+  serX.write(90);
+  serY.write(90);
+
 
 }
 void loop() {
 
 
-if(Serial.available()){ // only send data back if data has been sent
-   	
-	// read the incoming data
-	msg = Serial.readString();
+//  for (int i= 0; i<640; i=i+10){
+//     for(int j=0; j< 420; j=j+10){
+
+        msg = Serial.readString();
+
+        int j=random(0,420);
+        int i=random(0,640);
+        
+
+        Serial.println(j);
+
+        Serial.println(i);
+
+        angX = map (i, 0, 640, 30, 150);
+        angY = map (j, 0, 420, 30, 140);
+        serX.write(angX);
+        serY.write(angY);
+
+        Serial.println(angX);
+        Serial.println(angY);
+        
+        delay(50);      
+//        if(Serial.available()){ // only send data back if data has been sent    
+          if (msg!=oldMsg) {
+  
+            Serial.print(msg);
+            Serial.println("  ack"); 
+            msg.toCharArray(messaggio, 10);
+            
+            sscanf (messaggio,"(%d, %d)", &X, &Y);
+   
+            angX = map (X, 0, 640, 30, 150);
+            angY = map (Y, 0, 420, 60, 130);
+            
+            serX.write(angX);
+            serY.write(angY);
+        
+            delay(3000);
+            oldMsg=msg;
+        }
+       
+//     }
+//  }
 
 	// risponde positivamente alla ricezione
-  	if (msg) {
-  	  Serial.print(msg);
-  	  Serial.println("  ack"); 
-          msg.toCharArray(messaggio, 10);
-          
-          sscanf (messaggio,"(%d, %d)", &X, &Y);
- 
-          angX = map (X, 0, 640, 0, 180);
-          angY = map (Y, 0, 420, 0, 180);
-          
-	  serX.write(angX);
-	  serY.write(angY);         
-      }
-}
+//}
+
 delay(100); // delay for 1/10 of a second
 }
