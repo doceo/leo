3f9c428076d40cd7d50965a2d8367681330fa392
Doceo <diomede.mazzone@gmail.com>
2016-10-18 16:01:27 +0200

    Update leo.py
    aggiunti commenti, fatto pulizia del codice e scelta della webcam.

--- leo.py
+++ leo.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 
-
 import subprocess
 from face import *
 #from movimento import *
@@ -8,19 +7,34 @@ from seriale import *
 #import window
 from serverThread.py import *
 
+#libreria picamera e picamera.array servono a acquisire dalla webcam un formato array di tipo nunpy per essere trattato da openCV 
+from picamera.array import PiRGBArray
+from picamera import PiCamera
 
 #----------
 # M A I N
 #----------
 
-#avviamo il thread che in autonomia gestisce il server che si mette in ascolto di input
+#inizializzo una variabile per scegliere il tipo di acquisizione, se attiva la piCamera deve essere su "True"
+piCam = True
+
+#avviamo il thread che in autonomia ascolta possibili input esterni. i client di test sono nella cartella /client
 server = startThread(None)
 
-
-capture = cv.CaptureFromCAM(0)
+if piCam:
+	camera = PiCamera()
+	rawCapture = PiRGBArray(camera)
+ 
+	# allow the camera to warmup
+	time.sleep(0.1)
+ 
+# grab an image from the camera
+	camera.capture(rawCapture, format="bgr")
+	capture = rawCapture.array
+else:
+	capture = cv.CaptureFromCAM(0)
+	
 faceCascade = cv.Load("/usr/share/opencv/haarcascades/haarcascade_frontalface_alt.xml")
-
-distHold = 0
 
 while (cv.WaitKey(15)==-1):
     img = cv.QueryFrame(capture)
@@ -29,28 +43,17 @@ while (cv.WaitKey(15)==-1):
     if face:
 		print "ciao!"
 		
-		#il vettore face in quarta posizione (indice 3) contiene il 
-		#la lunghezza della diagonale
-		dist = face[3]
-		#print dist
-		
+		#stampa a monitor le coordinate del punto medio del volto
 		print face[2]
 
-		#il servo motore dell'assex 
-#		rotazione = gira(face[2])
-#		print rotazione
-		
-		#inviamo alla porta seriale le coordinate del punto medio
+		#inviamo alla porta seriale, quindi ad arduino, le coordinate del punto medio
 		invia(str(face[2]))
 		
-		if (dist < distHold - 9):
-			print "stai andando via!"
-		
-		distHold = dist
-		
+		#controlla il thread del server se ha ricevuto un input esterno
 		if(server.getValue()):
 		
 			invia(str(askThread(server)))
-		
+
+#chiude il server in ascolto
 server.stop()
 cv.ReleaseCapture(capture)
